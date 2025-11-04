f2f5a33e11ba7fc4cd65885b1536e06127d83287
joey collier <joey.collier.pro@gmail.com>
2025-11-04 09:31:18 +0100

    modif. to additional list

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+/* Déclarez un tableau listePhrases qui contient 3 courtes phrases : “Pas de panique !”, “La vie, l’univers et le reste”, “Merci pour le poisson”.
 
-for (let compteur = 0; compteur < 3; compteur = compteur + 1){
-  console.log(compteur)
+Demandez à l’utilisateur s’il veut la liste de mots ou la liste de phrases. Répétez la question tant que l’utilisateur n’a pas écrit “mots” ou “phrases”.
+
+Lancez votre boucle for, avec la liste que l’utilisateur a choisie.  */
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique", "l'univers, la vie et le reste", "Merci pour le poisson"]
+
+let score = 0
+
+for (let i = 0; i < listeMots.length; i++) {
+  let motUtilisateur = prompt(`Veuillez taper le mot suivant : ${listeMots[i]}`);
+
+  if (motUtilisateur === listeMots[i]) {
+      score++;
+  }
 };
 
-while (condition) {
-
-}
+console.log(`Votre score est de ${score}`);

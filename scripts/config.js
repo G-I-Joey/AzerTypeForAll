bb980284daefd97625e645e5bf67c414eb10f4f0
joey collier <joey.collier.pro@gmail.com>
2025-11-04 20:04:41 +0100

    create folder and dispatch & regroup codes on different files

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,4 @@
 /* Liste initiale de mots */
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-
 /* Déclarez un tableau listePhrases qui contient 3 courtes phrases : “Pas de panique !”, “La vie, l’univers et le reste”, “Merci pour le poisson”. */
 const listePhrases = ["Pas de panique", "l'univers, la vie et le reste", "Merci pour le poisson"]
---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+/* On demande à l’utilisateur de faire un CHOIX entre la liste de mots ou celle de phrases. Cette fonction ne prend pas de paramètres
+car c'est le user qui va rentrer lui meme  */
+function choisirPhraseOuMots() {
+  /* Tant que le user n'a pas choisi "mots" ou "phrases", on lui redemande */
+
+  let choix = prompt("Voulez-vous jouer avec la liste de mots ou la liste de phrases ? (écrivez 'mots' ou 'phrases')")
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Vous devez choisir entre 'mots' et 'phrases'")
+  }
+  return choix
+}
+
+function lancerBoucleDeJeu(listePropositions) {
+  score = 0
+  for (let i = 0; i < listePropositions.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+    if (motUtilisateur === listePropositions[i]) {
+      score++;
+    }
+  }
+  return score
+}
+
+function afficherResultat(score, nombreMotsTotal) {
+  /* afficherResultat : cette fonction prend en paramètre le résultat et le nombre total de mots proposés, et affiche le résultat du joueur ;  */
+  console.log("Votre résultat est de " + score + " sur " + nombreMotsTotal)
+}
+
+function lancerJeu() {
+  let choix = choisirPhraseOuMots()
+  /* On annote le score à 0 */
+  let score = 0
+  let nombreMotsTotal = 0
+
+  if (choix === 'mots') {
+    score = lancerBoucleDeJeu(listeMots)
+    nombreMotsTotal = listeMots.length
+  } else {
+    score = lancerBoucleDeJeu(listePhrases)
+    nombreMotsTotal = listePhrases.length
+  }
+
+  afficherResultat(score, nombreMotsTotal)
+}

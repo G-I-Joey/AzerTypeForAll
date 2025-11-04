a7c0926e8a0d402d2a55dd0a7ac50ed5fc13b293
joey collier <joey.collier.pro@gmail.com>
2025-11-04 11:53:46 +0100

    create file for the lists

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-/* Liste initiale de mots */
-
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-
-/* Déclarez un tableau listePhrases qui contient 3 courtes phrases : “Pas de panique !”, “La vie, l’univers et le reste”, “Merci pour le poisson”. */
-
-const listePhrases = ["Pas de panique", "l'univers, la vie et le reste", "Merci pour le poisson"]
 /* ON garde le scoring à 0 */
-
 let score = 0
 
 /* Demandez à l’utilisateur pour qu'il fasse le CHOIX entre la liste de mots ou la liste de phrases. Répétez la question tant que l’utilisateur n’a pas écrit “mots” ou “phrases”.*/
-
 let choix = prompt("Voulez-vous la liste de mots ou la liste de phrases ? (écrivez 'mots' ou 'phrases')");
 
 /* On fait en sorte que l'utilisateur puisse choisir exclusivement "mots" ou "phrases" jusqu'à ce qu'il l'ai correctement orthographié (boucle While) */
-
 while (choix !== "mots" && choix !== "phrases") {
   choix = prompt("Veuillez répondre par 'mots' ou 'phrases'. Voulez-vous la liste de mots ou la liste de phrases ?");
 }
 
 /* Lancez votre boucle for, en fonction de la liste que l’utilisateur a choisie.  */
-
 if (choix === "mots") {
   for (let i = 0; i < listeMots.length; i++) {
     let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])

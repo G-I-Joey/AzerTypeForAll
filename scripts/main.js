71061348254891f9d68a7b55eb94c82e82270dde
joey collier <joey.collier.pro@gmail.com>
2025-11-06 15:28:52 +0100

    modif. on index.html & main.js 's file

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
 /* On isole ici l'appel de la fonction pour exécuter l'ensemble des fonctions créées dans les différents fichiers */
-lancerJeu()
+/* lancerJeu() */
+
+/* Sélectionnez avec la méthode getElementById : l’input dans lequel le joueur va écrire son texte ;
+le bouton de validation. */
+let baliseEcritureZoneSaisie = document.getElementById("#inputEcriture");
+console.log(baliseEcritureZoneSaisie)

3a187746123fc0fd245fbdc0c0c46465faa6f942
joey collier <joey.collier.pro@gmail.com>
2025-11-06 16:37:16 +0100

    reinit html code without cloning teacher's work

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,2 @@
 /* On isole ici l'appel de la fonction pour exécuter l'ensemble des fonctions créées dans les différents fichiers */
 /* lancerJeu() */
-
-/* Sélectionnez avec la méthode getElementById : l’input dans lequel le joueur va écrire son texte ;
-le bouton de validation. */
-let baliseEcritureZoneSaisie = document.getElementById("#inputEcriture");
-console.log(baliseEcritureZoneSaisie)

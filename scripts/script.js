c0a37c9a47616d00a6b0feda012f0638270f384b
joey collier <joey.collier.pro@gmail.com>
2025-11-11 17:49:12 +0100

    verif. fonctions OK of the app

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@
 
  *********************************************************************************/
 
-
 let score = 0
 let nbMotsProposes = 0
-/* créez une fonction afficherResultat, qui va prendre en paramètre le score et le nombre de mots proposés, et afficher le score dans la zone dédiée */
+/* créez une fonction afficherResultat, qui va prendre en paramètre le score et le nombre de mots proposés,
+et afficher le score dans la zone dédiée */
 
 function afficherResultat(score, nbMotsProposes) {
 // Récupération de la zone dans laquelle on va écrire le score
@@ -41,24 +41,27 @@ function lancerJeu() {
   afficherResultat(score, nbMotsProposes)
 
   boutonValider.addEventListener("click", () => {
-/* ajoutez 1 à i à chaque fois que l’utilisateur clique sur le bouton Envoyer */
-    i++
-    afficherProposition(listeMots[i])
-
-    /* Si le mot numéro i du tableau vaut undefined, écrivez le message “Le jeu est fini” à la place du mot, et désactivez le bouton de validation.
-    Pour désactiver ce bouton, mettez la propriété disabled de ce bouton à true ; */
-    if (listeMots[i] === undefined) {
-      afficherProposition("Le jeu est fini !")
-      boutonValider.disabled = true
-    } else if (listeMots[i] && inputEcriture.value === listeMots[i - 1]) {
-      score++
-      afficherResultat(score, nbMotsProposes)
-    }
-
-/* à chaque fois que l’utilisateur clique sur Valider, videz le champ inputEcriture.  */
-    inputEcriture.value = ""
-
 /* faire apparaître dans la console le mot tapé dans la case dédiée 'inputEcriture' */
-    console.log(inputEcriture.value)
+  console.log(inputEcriture.value)
+  if (inputEcriture.value === listeMots[i]) {
+    score++
+  }
+/* ajoutez 1 à i à chaque fois que l’utilisateur clique sur le bouton Envoyer */
+  i++
+  /* à chaque fois que l’utilisateur clique sur Valider, videz le champ inputEcriture.  */
+  inputEcriture.value = ""
+  afficherProposition(listeMots[i])
+
+  /* Si le mot numéro i du tableau vaut undefined, écrivez le message “Le jeu est fini” à la place du mot, et désactivez le bouton de validation.
+  Pour désactiver ce bouton, mettez la propriété disabled de ce bouton à true ; */
+  if (listeMots[i] === undefined) {
+    afficherProposition("Le jeu est fini !")
+    boutonValider.disabled = true
+  } else {
+    afficherProposition(listeMots[i])
+  }
+
+  afficherResultat(score, nbMotsProposes)
+
   })
 }
